0c5bb19453858979b421e051f38519c4bc4e5e91
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-04-11 18:27:16 -0500

    Added client_append, init_list, optimized update directory

--- example-server/directory.c
+++ example-server/directory.c
@@ -10,19 +10,14 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 
-/*
-We need List structure that holds information about the clients.
-Each client can be a node in a linked list which holds the information
-about the cline including its IP address and its listening port.
-
-*/
-
+//Request types
 typedef enum {
 	CLIENT_JOIN,
 	REQUEST_NEW_PEER,
 	CLIENT_EXIT
 } request_t;
 
+//Client node
 typedef struct client_node
 {
 	struct client_node * next;
@@ -32,76 +27,108 @@ typedef struct client_node
 	size_t port; //holds listening port for client
 } client_node_t;
 
+//Client list 
 typedef struct client_list {
 	client_node_t * head; //pointer to list of client nodes
 	int cur_num_clients;
 } client_list_t;
 
+
+//Transmitted data packet
 typedef struct packet
 {
 	request_t request;
 	size_t client_id;
 	size_t port;
-}info_packet_t;
+} info_packet_t;
 
-int cur_num_clients;
+int cur_num_clients;  //tracks unique ids for clients
 size_t cid;
 client_list_t* dir_list;
 
 
+//Creates a new empty client node
 client_node_t* create_client_node(){
 	client_node_t * new_node = malloc (sizeof(client_node_t));
+	new_node->next = NULL;
 	if (new_node == NULL){
 		perror("Malloc failed!");
 	}
 	return new_node;
 }
 
-
 //List of possible parents
 client_list_t* return_list_clients(){
-	if (cur_num_clients == 0){
-		dir_list->head = NULL;
-		dir_list->cur_num_clients = 0;
-	}
 	return dir_list;
 }
 
+//Appends a new client node to the end of the list
+void append_node(client_node_t* node) {
 
-//Remove the client that wants to exit from the system
-void update_directory_server(size_t cid){
-	if (dir_list->head == NULL) {
+	if(dir_list->head == NULL) {
+		dir_list->head = node;
 		return;
 	}
 
 	client_node_t* cur = dir_list->head;
-	client_node_t* prev = dir_list->head;
 
-	if(cur->cid == cid) {
-   dir_list->head = prev->next;
-   free(prev);
-	}
+	//traverse to one node before end of the list
+	while(cur->next != NULL) {
+		cur = cur->next;
+	}  
 
-	while(cur->next != NULL || cur->next->cid != cid) {
-		prev = cur;
+	cur->next  = node; 
+}
+
+//Prints the cids of all the clients in our list (Only for testing purposes)
+void print_list(client_list_t* lst) {
+
+	client_node_t* cur = lst->head;
+
+	while(cur != NULL) {
+		printf("%d\n", (int) cur->cid);
 		cur = cur->next;
 	}
 
-	if (cur->next == NULL){
-		return;
-	}
+	return;
+}
 
-	prev = cur->next;
-	free(cur);
+//Removes the client that wants to exit from the system
+void update_directory_server(size_t cid){
+
+	client_node_t* cur = dir_list->head;
+	client_node_t* prev = dir_list->head;
+
+	while(cur != NULL) {
+
+		//if we find the cid we are looking for, just change the pointers
+		if(cur->cid == cid) {
+			if(cur == dir_list->head) { 
+				dir_list->head = dir_list->head->next;
+			} else {
+			//otherwise delete normally
+			prev->next = cur->next;
+		}
+			return;
+		}
+
+		//otherwise, keep traversing
+		prev = cur;
+		cur = cur->next;
+	}
+}
+
+void init_list() {
+	dir_list = malloc(sizeof(client_list_t));
+	dir_list->head = NULL;
 }
 
 int main(int argc, char const *argv[])
 {
 
-	dir_list = malloc (sizeof (client_list_t));
-	dir_list->head = NULL;
+	init_list();
 
-	//creates a socket
+/*	//creates a socket
 	int s = socket(AF_INET, SOCK_STREAM, 0);
 
 	//check if it fails to create
@@ -157,8 +184,10 @@ int main(int argc, char const *argv[])
 
 	printf("%d\n", (int)packet_info.port);
         close(s);
+
+        */
 	return 0; 
-	}
+}
 
 
 
