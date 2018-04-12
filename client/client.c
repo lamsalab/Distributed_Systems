0142be15148e90ec21775b91a18471e5b9307d96
Abyaya Lamsal <lamsalab@cs.grinnell.edu>
2018-04-11 21:41:51 -0500

    Client connects to ther server

--- client/client.c
+++ client/client.c
@@ -4,28 +4,50 @@
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <pthread.h>
 
 #include "ui.h"
 
+#define SERVER_PORT 6666
+
 //Request types
 typedef enum {
-	CLIENT_JOIN,
-	REQUEST_NEW_PEER,
-	CLIENT_EXIT
+  CLIENT_JOIN,
+  REQUEST_NEW_PEER,
+  CLIENT_EXIT
 } request_t;
 
 //Transmission data packet
 typedef struct packet
 {
-	request_t request;
-	size_t client_id;
-	size_t port;
+  request_t request;
+  size_t client_id;
+  size_t port;
+  char ipstr[INET_ADDRSTRLEN];
 } info_packet_t;
 
+//Thread args
 typedef struct _client_thread_args {
   int s;
 } client_thread_args_t;
+
+//Client node
+typedef struct client_node
+{
+  struct client_node * next;
+  size_t cid;
+  int client_num;
+  char ipstr[INET_ADDRSTRLEN]; //holds ip address of client
+  size_t port; //holds listening port for client
+} client_node_t;
+
+//Client list 
+typedef struct client_list {
+  client_node_t * head; //pointer to list of client nodes
+} client_list_t;
+
 
 
 void* client_fn(void* arg) {
@@ -43,6 +65,22 @@ void* client_fn(void* arg) {
   }
 }
 
+//Prints the cids of all the clients in our list (Only for testing purposes)
+void print_list(client_list_t* lst) {
+
+  client_node_t* cur = lst->head;
+
+  while(cur != NULL) {          
+    printf("cid: %zu\n",  cur->cid);
+    printf("port: %zu\n",  cur->port);
+    printf("ipstr: %s\n",  cur->ipstr);
+    cur = cur->next;
+    }
+
+  return;
+}
+
+
 int main(int argc, char** argv) {
   if(argc != 3) {
     fprintf(stderr, "Usage: %s <username> <server address>\n", argv[0]);
@@ -52,6 +90,39 @@ int main(int argc, char** argv) {
   char* local_user = argv[1];
   char* server_address = argv[2];
 
+  //Determine which port this client is listening on
+  int s_client = socket(AF_INET, SOCK_STREAM, 0);
+  if(s_client == -1) {
+    perror("socket failed");
+    exit(2);
+  }
+
+  //Assigns random port
+  struct sockaddr_in client_addr = {
+    .sin_family = AF_INET,
+    .sin_port = 0
+  };
+
+  //bind socket to address
+  if(bind(s_client, (struct sockaddr*)&client_addr, sizeof(struct sockaddr_in))) {
+    perror("bind failed");
+    exit(2);
+  }
+
+  //Got the idea from https://stackoverflow.com/questions/4046616/sockets-how-to-find-out-what-port-and-address-im-assigned
+  //Get the port no.
+  socklen_t len = sizeof(client_addr);
+  int port_no;
+  char ipstr[INET_ADDRSTRLEN];
+  if (getsockname(s_client, (struct sockaddr *)&client_addr, &len) == -1){
+    perror("getsockname");
+    exit(2);
+  }
+  port_no = ntohs(client_addr.sin_port);
+  inet_ntop(AF_INET, &client_addr.sin_addr, ipstr, INET_ADDRSTRLEN);
+
+
+  //Getting the host name for the server
   struct hostent* server;
   server = gethostbyname(server_address);
   if(server == NULL) {
@@ -59,68 +130,78 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  int s = socket(AF_INET, SOCK_STREAM, 0);
-  if(s == -1) {
+  //Create a socket for the server
+  int s_server = socket(AF_INET, SOCK_STREAM, 0);
+  if(s_server == -1) {
     perror("socket failed");
     exit(2);
   }
 
+  //Server socket address
   struct sockaddr_in addr = {
     .sin_family = AF_INET,
-    .sin_port = htons(4446)
+    .sin_port = htons(SERVER_PORT)
   };
   
-
+  //Copy server address
   bcopy((char*)server->h_addr, (char*)&addr.sin_addr.s_addr, server->h_length);
 
-  if(connect(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
+  //Connect with the server
+  if(connect(s_server, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
     perror("connect failed");
     exit(2);
   }
 
-request_t request = CLIENT_EXIT;
+  //Packet to be sent to the server
+  info_packet_t packet;
+  packet.request = CLIENT_JOIN;
+  packet.port = port_no;
+  strcpy(packet.ipstr, ipstr);
 
-info_packet_t packet = 
-   {
-   .request = request,
-   .port = 4445
-  };
+//Send the packet to the server
+  send(s_server,(void *)&packet, sizeof(packet), 0);
 
-  send(s,(void *)&packet, sizeof(packet), 0);
-
- /* pthread_t client_thread;
-  client_thread_args_t args_client;
-  args_client.s = s;
-
-  //A thread to accept connections from other clients
-  if(pthread_create(&client_thread, NULL, client_fn, &args_client)) {
-    perror("pthread_create failed");
+  client_list_t potential_parents;
+  //Get the client information from the connecting client node
+  if (recv(s_server, (client_list_t*) &potential_parents, sizeof(potential_parents), 0) < 0){
+    perror("There was a problem in reading the data\n");
     exit(2);
   }
+  print_list(&potential_parents);
 
-  pthread_join(client_thread, NULL); */
+  /* pthread_t client_thread;
+     client_thread_args_t args_client;
+     args_client.s = s;
+
+     //A thread to accept connections from other clients
+     if(pthread_create(&client_thread, NULL, client_fn, &args_client)) {
+     perror("pthread_create failed");
+     exit(2);
+     }
+
+     pthread_join(client_thread, NULL); */
 
   // Initialize the chat client's user interface.
- /* ui_init();
+  /* ui_init();
   
   // Add a test message
   ui_add_message(NULL, "Type your message and hit <ENTER> to post.");
   
   // Loop forever
   while(true) {
-    // Read a message from the UI
-    char* message = ui_read_input();
+  // Read a message from the UI
+  char* message = ui_read_input();
     
-    // If the message is a quit command, shut down. Otherwise print the message
-    if(strcmp(message, "\\quit") == 0) {
-      break;
-    } else if(strlen(message) > 0) {
-      // Add the message to the UI
-      ui_add_message(local_user, message);
-    }
+  // If the message is a quit command, shut down. Otherwise print the message
+  if(strcmp(message, "\\quit") == 0) {
+  break;
+  } else if(strlen(message) > 0) {
+  // Add the message to the UI
+  ui_add_message(local_user, message);
+  }
     
-    // Free the message
-    free(message);
+  // Free the message
+  free(message);
   }
   
   // Clean up the UI
--- example-server/directory.c
+++ example-server/directory.c
@@ -10,181 +10,213 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 
+#define SERVER_PORT 6666
+
 //Request types
 typedef enum {
-	CLIENT_JOIN,
-	REQUEST_NEW_PEER,
-	CLIENT_EXIT
+  CLIENT_JOIN,
+  REQUEST_NEW_PEER,
+  CLIENT_EXIT
 } request_t;
 
 //Client node
 typedef struct client_node
 {
-	struct client_node * next;
-	size_t cid;
-	int client_num;
-	char ipstr[INET_ADDRSTRLEN]; //holds ip address of client
-	size_t port; //holds listening port for client
+  struct client_node * next;
+  size_t cid;
+  int client_num;
+  char ipstr[INET_ADDRSTRLEN]; //holds ip address of client
+  size_t port; //holds listening port for client
 } client_node_t;
 
 //Client list 
 typedef struct client_list {
-	client_node_t * head; //pointer to list of client nodes
-	int cur_num_clients;
+  client_node_t * head; //pointer to list of client nodes
+  int cur_num_clients;
 } client_list_t;
 
 
 //Transmitted data packet
 typedef struct packet
 {
-	request_t request;
-	size_t client_id;
-	size_t port;
+  request_t request;
+  size_t client_id;  
+  size_t port;
+  char ipstr[INET_ADDRSTRLEN];
 } info_packet_t;
 
-int cur_num_clients;  //tracks unique ids for clients
-size_t cid;
+//List of potential parents for new clients
+typedef struct parent_list
+{
+  client_list_t *parents;
+}parent_list_t;
+
+int cur_num_clients = 0;  //tracks unique ids for clients
 client_list_t* dir_list;
 
 
 //Creates a new empty client node
-client_node_t* create_client_node(){
-	client_node_t * new_node = malloc (sizeof(client_node_t));
-	new_node->next = NULL;
-	if (new_node == NULL){
-		perror("Malloc failed!");
-	}
-	return new_node;
+client_node_t* create_client_node(info_packet_t * client_packet){
+  client_node_t * new_node = malloc (sizeof(client_node_t));
+  new_node->next = NULL;        
+  if (new_node == NULL){
+    perror("Malloc failed!");
+  }
+  new_node->cid = cur_num_clients++;
+  new_node->port = client_packet->port;
+  strcpy(new_node->ipstr, client_packet->ipstr);
+  return new_node;
 }
 
 //List of possible parents
 client_list_t* return_list_clients(){
-	return dir_list;
+  return dir_list;
 }
 
 //Appends a new client node to the end of the list
 void append_node(client_node_t* node) {
 
-	if(dir_list->head == NULL) {
-		dir_list->head = node;
-		return;
-	}
+  if(dir_list->head == NULL) {
+    dir_list->head = node;
+    return;
+  }
 
-	client_node_t* cur = dir_list->head;
+  client_node_t* cur = dir_list->head;
 
-	//traverse to one node before end of the list
-	while(cur->next != NULL) {
-		cur = cur->next;
-	}  
+  //traverse to one node before end of the list
+  while(cur->next != NULL) {
+    cur = cur->next;
+  }  
 
-	cur->next  = node; 
+  cur->next  = node; 
 }
 
+
+/*
 //Prints the cids of all the clients in our list (Only for testing purposes)
 void print_list(client_list_t* lst) {
 
-	client_node_t* cur = lst->head;
+  client_node_t* cur = lst->head;
 
-	while(cur != NULL) {
-		printf("%d\n", (int) cur->cid);
-		cur = cur->next;
-	}
+  while(cur != NULL) {          
+    printf("cid: %zu\n",  cur->cid);
+    printf("port: %zu\n",  cur->port);
+    printf("ipstr: %s\n",  cur->ipstr);
+    cur = cur->next;
+  }
 
-	return;
+  return;
 }
+*/
 
 //Removes the client that wants to exit from the system
 void update_directory_server(size_t cid){
 
-	client_node_t* cur = dir_list->head;
-	client_node_t* prev = dir_list->head;
+  client_node_t* cur = dir_list->head;
+  client_node_t* prev = dir_list->head;
 
-	while(cur != NULL) {
+  while(cur != NULL) {
 
-		//if we find the cid we are looking for, just change the pointers
-		if(cur->cid == cid) {
-			if(cur == dir_list->head) { 
-				dir_list->head = dir_list->head->next;
-			} else {
-			//otherwise delete normally
-			prev->next = cur->next;
-		}
-			return;
-		}
+    //if we find the cid we are looking for, just change the pointers
+    if(cur->cid == cid) {
+      if(cur == dir_list->head) { 
+        dir_list->head = dir_list->head->next;
+      } else {
+        //otherwise delete normally
+        prev->next = cur->next;
+      }
+      return;
+    }
 
-		//otherwise, keep traversing
-		prev = cur;
-		cur = cur->next;
-	}
+    //otherwise, keep traversing
+    prev = cur;
+    cur = cur->next;
+  }
 }
 
 void init_list() {
-	dir_list = malloc(sizeof(client_list_t));
-	dir_list->head = NULL;
+  dir_list = malloc(sizeof(client_list_t));
+  dir_list->head = NULL;
 }
 
 int setup_server() {
-	//creates a socket
-	int s = socket(AF_INET, SOCK_STREAM, 0);
+  //creates a socket
+  int s = socket(AF_INET, SOCK_STREAM, 0);
 
-	//check if it fails to create
-	if(s == -1) {
-	  perror("socket failed");
-	  exit(2);
-	}
+  //check if it fails to create
+  if(s == -1) {
+    perror("socket failed");
+    exit(2);
+  }
 
-	//set up an internet socket address where server listens to 
-	struct sockaddr_in addr;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(4446);
+  //set up an internet socket address where server listens to 
+  struct sockaddr_in addr;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(SERVER_PORT);
 
-	//bind socket to address
-	if(bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
-	  perror("bind failed");
-	  exit(2);
-	}
+  //bind socket to address
+  if(bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
+    perror("bind failed");
+    exit(2);
+  }
 
-	//begin listening
-	if(listen(s, 2)) {
-	  perror("listen failed");
-	  exit(2);
-	}
+  //begin listening
+  if(listen(s, 2)) {
+    perror("listen failed");
+    exit(2);
+  }
 
-    return s;
+  return s;
 }
 
 int main(int argc, char const *argv[]) {
 
-	init_list();
-	int s = setup_server();
+  init_list();     
+  int s = setup_server();
 
-	//accept an incoming connection
-	struct sockaddr_in client_addr;
-	socklen_t client_addr_length = sizeof(struct sockaddr_in);
-	int client_socket = accept(s, (struct sockaddr*)&client_addr, &client_addr_length);
+  //accept an incoming connection
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_length = sizeof(struct sockaddr_in);
+  int client_socket = accept(s, (struct sockaddr*)&client_addr, &client_addr_length);
 
-	if(client_socket == -1) {
-	  perror("accept failed");
-	  exit(2);
-	}
+  if(client_socket == -1) {
+    perror("accept failed");
+    exit(2);
+  }
 
- 	//Get the client information from the connecting client node
-	info_packet_t packet_info;
-	if (recv(client_socket, (info_packet_t*) &packet_info, sizeof(packet_info), 0) < 0){
-        perror("There was a problem in reading the data\n");
-        exit(2);
-	}
+  //Get the client information from the connecting client node
+  info_packet_t packet_info;
+  if (recv(client_socket, (info_packet_t*) &packet_info, sizeof(packet_info), 0) < 0){
+    perror("There was a problem in reading the data\n");
+    exit(2);
+  }
 
-	if(packet_info.request == CLIENT_JOIN) {
-		printf("Request type: CLIENT_JOIN\n");
-	}
-	printf("Port number is %d\n", (int)packet_info.port);
+  switch (packet_info.request){
+  case CLIENT_JOIN:
+    //Add the clients;
+    append_node(create_client_node(&packet_info));
+    break;
+  case REQUEST_NEW_PEER:
+    ;//New parents;
+  case CLIENT_EXIT:
+    ;//Update the list
+  }
+
+  client_list_t parent_list = *dir_list;
+  print_list(&parent_list);
+
+  parent_list = *dir_list;
+  
+
+  
+  //Send the packet to the client
+  send(client_socket,(void *)&parent_list, sizeof(client_list_t), 0);
         
-    close(s);
+  close(s);
 
        
-	return 0; 
+  return 0; 
 }
 
 
